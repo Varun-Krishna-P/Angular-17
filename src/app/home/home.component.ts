21c971633d1b24c04a2aab77f1f66d47833f2c50
Varun Krishna. P <Varunkrishna.dbz@gmail.com>
2025-01-02 21:37:35 +0530

    feat: add dynamic values to template pass from parent to child

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  title = 'I am from home component'
+  title = 'inside home'
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: Housinglocation = {
+    id: 999,
+    name: 'house 1',
+    city: 'Chennai',
+    state: 'Tamil Nadu',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false
+  };
 
 }
