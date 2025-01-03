86a57fe5d0a9d3b65cce01c8b8550d8bf80894c7
Varun Krishna. P <Varunkrishna.dbz@gmail.com>
2025-01-03 20:29:43 +0530

    feat: adding housing service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { Housinglocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../services/housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  title = 'inside home'
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: Housinglocation = {
-    id: 999,
-    name: 'house 1',
-    city: 'Chennai',
-    state: 'Tamil Nadu',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false
-  };
+  title = 'inside home';
+  housingLocationList!: Housinglocation[];
+  // housingLocationList: HousingLocation[] = [];
+  // housingService: HousingService = inject(HousingService);
+  constructor(private _housingService: HousingService) {
+    this.housingLocationList = this._housingService.getAllHousingLocations();
+  }
 
 }
